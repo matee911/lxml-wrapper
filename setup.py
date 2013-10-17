ba243fef295f4b81cd853264ca8265052b6ff5e2
matee <matee@matee.net>
2013-10-17 10:58:41 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='lxml-wrapper',
       description='lxml wrapper that simplifies xml generation code.',
       keywords='lxml wrapper dsl',
-      version='0.3.5',
+      version='0.3.6',
       license='BSD',
       url='http://github.com/matee911/lxml-wrapper', # home page for the package
       download_url='http://github.com/matee911/lxml-wrapper/downloads',
@@ -13,9 +13,9 @@ setup(name='lxml-wrapper',
       author_email='matee@matee.net',
       long_description='''
       This wrapper simplifies your Python xml generation code.
-      
+
       Example XML:
-      
+
       <root attrib="10">
         text
         <child attrib="">
@@ -23,32 +23,32 @@ setup(name='lxml-wrapper',
         </child>
         tail
       </root>
-      
+
       New way:
-      
+
       E('root', attrib=10).add(
-          'text', 
+          'text',
           E('child', attrib=None).add(
-              'childtext'), 
+              'childtext'),
           'tail')
-      
+
       Old way:
-      
+
       root = Element('root', attrib=str(10)) # cast to str
       root.text = 'text'
       child = SubElement(root, 'child', attrib=value or "") # change None to empty string
       child.text = 'childtext'
       child.tail = 'tail'
-      
-      
+
+
       Now with .add_if and .add_for methods:
-      
+
       E('root').add_if(1==1, E('child')) -> <root><child /></root>
-      
+
       E('root').add_if(1==0, E('child')) -> <root/>
-      
+
       E('root').add_for([1,2], lambda item: E('item', attr=item)) -> <root><item attr="1"/><item attr="2"/></root>
-      
+
       ''',
       requires=['lxml'],
       #package_dir={'':'src'},
@@ -61,3 +61,4 @@ setup(name='lxml-wrapper',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
       ],)
+
